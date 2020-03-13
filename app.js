b480c97afec1ce4ef075410d0a17a2849d9b6a2d
unknown <hpcgg@foxmail.com>
2020-03-13 18:00:42 +0800

    allow cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const Koa=require('koa');
 const Router=require('koa-router');
 // const mongoose = require('mongoose');
 const ModelDb = require('./db');
+const cors = require('koa2-cors');
 var bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router =new Router();
 app.use(bodyParser());
+app.use(cors());
 //配置路由
 app.use(router.routes()).use(router.allowedMethods());
 const user=require('./routes/api/user');
